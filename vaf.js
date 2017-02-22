72ae2e4a3454bf4dd25dfa6042eebb0f10e38b16
Eduard Zora <edwardwow07@gmail.com>
2017-02-21 20:01:27 -0500

    Granja con Canvas y js version1

---
+++ vaf.js
@@ -0,0 +1,86 @@
+var vp = document.getElementById("villaAF");
+var papel = vp.getContext("2d");
+
+var fondo = {
+  url: "img/tile.png",
+  cargaOK: false
+}
+var vaca = {
+  url: "img/vaca.png",
+  cargaOK: false
+};
+
+var cerdo = {
+  url: "img/cerdo.png",
+  cargaOK: false
+};
+
+var cantidad = aleatorio(1, 10);
+
+fondo.imagen = new Image();
+fondo.imagen.src = fondo.url;
+fondo.imagen.addEventListener("load", cargarFondo);
+
+vaca.imagen = new Image();
+vaca.imagen.src = vaca.url;
+vaca.imagen.addEventListener("load", cargarVacas);
+
+cerdo.imagen = new Image();
+cerdo.imagen.src = cerdo.url;
+cerdo.imagen.addEventListener("load", cargarCerdo);
+
+function cargarFondo()
+{
+  fondo.cargaOK = true;
+  dibujar();
+}
+function cargarVacas()
+{
+  vaca.cargaOK = true;
+  dibujar();
+}
+
+function cargarCerdo()
+{
+  cerdo.cargaOK = true;
+  dibujar();
+}
+
+function dibujar()
+{
+  if(fondo.cargaOK)
+  {
+    papel.drawImage(fondo.imagen, 0, 0);
+  }
+  if(vaca.cargaOK)
+  {
+    console.log(cantidad);
+    for(var v=0; v < cantidad; v++)
+    {
+      var x = aleatorio(0, 7);
+      var y = aleatorio(0, 10);
+      var x = x * 60;
+      var y = y * 40;
+      papel.drawImage(vaca.imagen, x, y);
+    }
+  }
+  if(cerdo.cargaOK)
+  {
+    console.log(cantidad);
+    for(var v=0; v < cantidad; v++)
+    {
+      var x = aleatorio(0, 7);
+      var y = aleatorio(0, 10);
+      var x = x * 60;
+      var y = y * 40;
+      papel.drawImage(cerdo.imagen, x, y);
+    }
+  }
+}
+function aleatorio(min, maxi)
+{
+  /*Hacer números aleatorios.*/
+  var resultado;
+  resultado = Math.floor(Math.random() * (maxi - min + 1)) + min;
+  return resultado;
+}
